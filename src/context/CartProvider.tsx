92aff8df68ea8d63b340ad68b1bd02733ab47337
EmraanIqbal <se.imraniqbal@gmail.com>
2023-02-05 13:05:30 +0500

    CartProvider has been completed

---
+++ src/context/CartProvider.tsx
@@ -0,0 +1,121 @@
+import { createContext, ReactElement, useMemo, useReducer } from "react";
+
+export type cartItemType = {
+    sku: string;
+    name: string;
+    price: number;
+    qty: number
+}
+
+type CartStateType = { cart: cartItemType[] }
+
+const initCartState: CartStateType = { cart: [] }
+
+const REDUCER_ACTION_TYPE = {
+    ADD: "ADD",
+    REMOVE: "REMOVE",
+    QUANTITY: "QUANTITY",
+    SUBMIT: "SUBMIT"
+}
+
+export type ReducerActionType = typeof REDUCER_ACTION_TYPE
+
+export type ReducerAction = {
+    type: string
+    payload?: cartItemType
+}
+
+const reducer = (state: CartStateType, action: ReducerAction): CartStateType => {
+    switch (action.type) {
+        case REDUCER_ACTION_TYPE.ADD: {
+            if (!action.payload) {
+                throw new Error("Action.Payload is missing in ADD")
+            }
+            const { sku, price, name } = action.payload
+            const filteredCart: cartItemType[] = state.cart.filter(item => item.sku !== sku)
+            const itemExists: cartItemType | undefined = state.cart.find(item => item.sku === sku);
+            const qty: number = itemExists ? itemExists.qty + 1 : 1
+            return { ...state, cart: [...filteredCart, { sku, name, price, qty }] }
+        }
+        case REDUCER_ACTION_TYPE.REMOVE: {
+            if (!action.payload) {
+                throw new Error("Action.Payload is missing in REMOVE")
+            }
+
+            const { sku } = action.payload
+            const filteredCart: cartItemType[] = state.cart.filter(item => item.sku !== sku)
+
+            return { ...state, cart: [...filteredCart] }
+
+        }
+        case REDUCER_ACTION_TYPE.QUANTITY: {
+            if (!action.payload) {
+                throw new Error("Action.Payload is missing in QUANTITY")
+            }
+
+            const { sku, qty } = action.payload
+            const itemExists: cartItemType | undefined = state.cart.find(item => item.sku === sku);
+            if (!itemExists) {
+                throw new Error("Item didnit Exits")
+            }
+            const updatedItem: cartItemType = { ...itemExists, qty }
+            const filteredCart: cartItemType[] = state.cart.filter(item => item.sku !== sku)
+
+            return { ...state, cart: [...filteredCart, updatedItem] }
+
+        }
+        case REDUCER_ACTION_TYPE.SUBMIT: {
+            return { ...state, cart: [] }
+        }
+        default:
+            throw new Error("An Error is Occured")
+    }
+}
+
+const useCartContext = (initCartState: CartStateType) => {
+    const [state, dispatch] = useReducer(reducer, initCartState)
+
+    const REDUCER_ACTIONS = useMemo(() => {
+        return REDUCER_ACTION_TYPE
+    }, [])
+
+    const totalItems: number = state.cart.reduce((prev, nex) => {
+        return prev + nex.qty
+    }, 0)
+
+    const totalPrice = new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(state.cart.reduce((prev, current) => {
+        return prev + (current.qty * current.price)
+    }, 0))
+
+    const cart = state.cart.sort((a, b) => {
+        const itemA = Number(a.sku.slice(-4))
+        const itemB = Number(b.sku.slice(-4))
+        return itemA - itemB
+    })
+
+    return { dispatch, REDUCER_ACTIONS, totalItems, totalPrice, cart }
+}
+
+export type UerCartContextType = ReturnType<typeof useCartContext>
+
+const initCartContextState: UerCartContextType = {
+    dispatch: () => { },
+    REDUCER_ACTIONS: REDUCER_ACTION_TYPE,
+    totalItems: 0,
+    totalPrice: "",
+    cart: []
+}
+
+export const CartContext = createContext<UerCartContextType>(initCartContextState)
+
+type ChildrenType = {
+    children?: ReactElement | ReactElement[]
+}
+
+export const CartProvider = ({ children }: ChildrenType): ReactElement => {
+    return (
+        <CartContext.Provider value={useCartContext(initCartState)}>{children}</CartContext.Provider>
+    )
+}
+
+export default CartProvider
